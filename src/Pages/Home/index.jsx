8c96affbcb8674b72a24e7d90a8a4d9b77a8ca59
Markéta Mížová <marketa.mizova@gmail.com>
2023-05-26 11:11:00 +0200

    html pro Pages/Home

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+
+export const Home =() => {
+    return(
+        <main class="container">
+        <header>
+          <menu>
+            <ul>
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Markéta</a></li>
+              <li><a href="#">Alex</a></li> 
+            </ul>
+          </menu>
+        </header>
+  
+        <article class="group">
+         
+          <img src="./img/iStock-1426547978.jpg" alt="hands" />
+     
+  
+          <div class="text">
+            <h3>Frontend Ladies</h3>
+            <p>
+              Web, který pomůže propojit firmy a nové juniorní frontend vývojářky.
+              Samy víme, jak je těžké sebrat odvahu a poslat CV na naši vysněnou
+              pozici v IT - ještě toho moc neumím, vždyť se mi vysmějí, tenhle
+              skill ani nemám, raději nikam CV posílat nebudu. Budeme rády, pokud
+              se nám podaří náš web zpropagovat ve firmách spolupracujících s
+              Czechitas, které tak budou mít možnost podívat se na portfolia
+              jednotlivých absolventek digitálních akademií a sami je oslovit s
+              nabídkou práce, pozvat na pohovor.
+            </p>
+           
+          </div>
+        </article>
+  
+        <footer>
+          <small class="right"> © Czechitas 2023 </small>
+          <small class="left"> Frontend Ladies - O projektu </small>
+        </footer>
+      </main>
+    )
+}
