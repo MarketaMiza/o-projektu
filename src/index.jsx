35be077fa125b52b223074248fdb3e05055a4f62
Markéta Mížová <marketa.mizova@gmail.com>
2023-05-26 10:59:33 +0200

    pridani Pages a komponent / Home, Marketa, Alex

--- src/index.jsx
+++ src/index.jsx
@@ -1,28 +1,62 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import './style.css';
+// import React from 'react';
+// import { createRoot } from 'react-dom/client';
+// import './style.css';
+// import {
+//   createBrowserRouter,
+//   RouterProvider,
+//   Link,
+//   Outlet,
+// } from "react-router-dom";
 
-const App = () => {
-  return (
-    <div className="container">
-      <header>
-        <div className="logo" />
-        <h1>React webová aplikace</h1>
-      </header>
-      <main>
-        <p>
-          Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí
-          <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>
-          .
-        </p>
-      </main>
-      <footer>
-        <p>Czechitas, Digitální akademie: Web</p>
-      </footer>
-    </div>
-  );
-};
 
-createRoot(
-  document.querySelector('#app'),
-).render(<App />);
+// import { Home } from "./pages/Home/index";
+// import { About } from "./pages/About/index";
+// import './style.css'
+
+// const App = () => {
+//   return (
+//     <div className="container">
+//       <nav>
+//         <Link to="/Home">domu</Link>
+//         <span> | </span>
+//         <Link to="/About">about</Link>
+     
+//       <span> | </span>
+//         <Link to="/CentersPage">pobocky</Link>
+//       </nav>
+//       <Outlet />
+//     </div>
+//   );
+// };
+
+// const ErrorPage = () => {
+//   return (
+//     <main>
+//       <h2>404: Tady nic není</h2>
+//       <p>Asi jste se spletli</p>
+//     </main>
+//   );
+// };
+
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     errorElement: <ErrorPage />,
+//     children: [
+//       {
+//         path: "/Home",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/About",
+//         element: <About />,
+//       },
+//     ],
+//   },
+// ]);
+
+// createRoot(document.querySelector("#app")).render(
+//   <RouterProvider router={router} />
+// );
